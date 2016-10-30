f48767f194542bb2a8e93479e3c5e002dd608b94
Franziska von der Goltz <franziska@vdgoltz.eu>
2016-10-30 23:04:25 +0000

    completed all parts of the assessment

--- query.py
+++ query.py
@@ -35,25 +35,47 @@ Brand.query.filter(Brand.founded > 1920).all()
 Model.query.filter(Model.name.like("Cor%")).all()
 
 # Get all brands that were founded in 1903 and that are not yet discontinued.
+Brand.query.filter(Brand.founded == 1903, Brand.discontinued == None).all()
 
 # Get all brands that are either 1) discontinued (at any time) or 2) founded
 # before 1950.
+Brand.query.filter( (Brand.discontinued != None) | (Brand.founded < 1950) ).all()
 
 # Get all models whose brand_name is not Chevrolet.
+Model.query.filter(Model.name != "Chevrolet").all()
 
 # Fill in the following functions. (See directions for more info.)
+
 
 def get_model_info(year):
     '''Takes in a year, and prints out each model, brand_name, and brand
     headquarters for that year using only ONE database query.'''
 
-    pass
+    matches = Model.query.filter(Model.year == year).all()
+
+    if matches:
+        for match in matches:
+            print "Model Name: ", match.name
+            print "Brand Name: ", match.brand_name
+            print "Brand HQ: ", match.brand.headquarters
+    else:
+        "No data found for that year."
+
 
 def get_brands_summary():
     '''Prints out each brand name, and each model name for that brand
      using only ONE database query.'''
 
-    pass
+    brands = Brand.query.all()
+
+    brands_and_models = {}
+
+    for brand in brands:
+        brands_and_models[brand.name] = []
+        for model in brand.models:
+            brands_and_models[brand.name].append(model.name)
+        print brand.name, ": ", brands_and_models[brand.name]
+
 
 # -------------------------------------------------------------------
 # Part 2.5: Discussion Questions (Include your answers as comments.)
@@ -61,15 +83,32 @@ def get_brands_summary():
 # 1. What is the returned value and datatype of
 # ``Brand.query.filter_by(name='Ford')``?
 
+# The returned value is: <flask_sqlalchemy.BaseQuery at 0x7f5bf807bd50>
+# This is a list with one database object, representing the query we made,
+# not the values associated with the query in the database. Having a base query
+# object allows us to reuse the objects and access their properties. No results
+# will be shown until we 'collect' data, with one(), first(), get(), or all(). 
+
 # 2. In your own words, what is an association table, and what *type* of
 # relationship does an association table manage?
+
+# An association table is a sort of bridge table. It is the connection between
+# two tables that need to be connetced, but because "many to many" relationships
+# are not technically possible, an association table is created.
+# The main difference between an association table and a common table, is that an association
+# table does not have any meaningful fields. It's sole purpose is to link two other
+# tables together.
 
 # -------------------------------------------------------------------
 # Part 3
 
 def search_brands_by_name(mystr):
-    pass
+    """returns a list of objects with the brands containing / are equal to the passed string"""
+
+    return Brand.query.filter(Brand.name.like("%" + mystr + "%")).all()
 
 
 def get_models_between(start_year, end_year):
-    pass
+    """returns a list of objects with models between two years"""
+
+    return Model.query.filter(Model.year >= start_year, Model.year < end_year).all()
